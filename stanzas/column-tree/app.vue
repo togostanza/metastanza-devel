4c0046820d218cad7191875e21ae1bdfbd7999d7
Yukiko Noda <yukiko.noda@penqe.com>
2022-12-06 14:39:30 +0900

    make responseJSON an array and remove the error

--- stanzas/column-tree/app.vue
+++ stanzas/column-tree/app.vue
@@ -93,7 +93,7 @@ export default defineComponent({
       nodeShowBorders: isTruthBool(params?.nodeShowBorders?.value),
       nodeContentAlignment: params?.nodeContentAlignment?.value,
       showSuggestions: false,
-      responseJSON: null,
+      responseJSON: [],
       columnData: [],
       checkedNodes: new Map(),
       searchTerm: "",
