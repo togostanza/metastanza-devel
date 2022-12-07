3ef3717d467722186ceffa592abbf1987e9ab369
Yukiko Noda <yukiko.noda@penqe.com>
2022-12-07 18:40:33 +0900

    implement loadData

--- stanzas/text/index.js
+++ stanzas/text/index.js
@@ -54,14 +54,14 @@ export default class Text extends Stanza {
   async render() {
     const main = this.root.querySelector("main");
 
-    this._dataset = await this._loadText(this.params["data-url"], main);
-
-    // const value = this._dataset;
     const value = await loadData(
       this.params["data-url"],
       this.params["data-type"],
       main
     );
+
+    this._dataset = value;
+
     if (this._isMarkdownMode()) {
       const parser = new commonmark.Parser();
       const renderer = new commonmark.HtmlRenderer();
@@ -75,7 +75,6 @@ export default class Text extends Stanza {
       main.querySelectorAll("pre code").forEach((el) => {
         hljs.highlightElement(el);
       });
-      console.log("MATH", main);
       renderMathInElement(main);
     } else {
       const text = this._dataset;
@@ -89,51 +88,5 @@ export default class Text extends Stanza {
 
     appendCustomCss(this, this.params["custom_css_url"]);
     appendCustomCss(this, this.params["highlight-css-url"]);
-  }
-
-  async _loadText(url, main) {
-    const spinnerDiv = document.createElement("div");
-
-    Object.assign(spinnerDiv, {
-      className: "metastanza-loading-icon-div",
-      id: "metastanza-loading-icon-div",
-    });
-    spinnerDiv.style = `
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    `;
-
-    // const spinnerImg = document.createElement("img");
-    // Object.assign(spinnerImg, {
-    //   className: "metastanza-loading-icon",
-    //   id: "metastanza-loading-icon",
-    //   src: spinner,
-    // });
-
-    // spinnerImg.style = `
-    // width: 30px;
-    // height: auto;
-    // display: block;
-    // `;
-
-    // spinnerDiv.appendChild(spinnerImg);
-    main.appendChild(spinnerDiv);
-
-    const response = await fetch(url).then((res) => res.text());
-    //error処理の追加 fetchなど status
-
-    // Parameters (heatmapの様に行いたい)
-    // const dataset = await loadData(
-    //   this.params["data-url"],
-    //   this.params["data-type"],
-    //   root
-    // );
-
-    //togostanza utilsを追加する
-
-    main.removeChild(spinnerDiv);
-
-    return response;
   }
 }
