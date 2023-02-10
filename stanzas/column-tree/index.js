bf51483418fd39ddb71859b21acdc8fddfbe5978
Yukiko Noda <yukiko.noda@penqe.com>
2023-02-10 12:22:10 +0900

    rename togostanza-custom_css_url

--- stanzas/column-tree/index.js
+++ stanzas/column-tree/index.js
@@ -20,7 +20,7 @@ export default class ColumnTree extends Stanza {
   }
 
   async render() {
-    appendCustomCss(this, this.params["custom_css_url"]);
+    appendCustomCss(this, this.params["togostanza-custom_css_url"]);
 
     const main = this.root.querySelector("main");
 
