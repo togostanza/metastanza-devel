7898e096e49e9601a99e847a09310af4d27e7ce4
行事光 <h.gyoji.vrds@gmail.com>
2025-06-30 15:35:28 +0900

    fix lint error

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from "eslint/config";
 import vue from "eslint-plugin-vue";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import vueParser from "vue-eslint-parser";
 import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   {
-    files: ["**/*.js", "**/*.ts", "**/*.tsx", "**/*.vue"],
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     ignores: [".github", "dist"],
     languageOptions: {
       ecmaVersion: 2021,
@@ -22,15 +23,44 @@ export default defineConfig([
       },
     },
     plugins: {
-      vue,
       "@typescript-eslint": tseslint,
     },
-    settings: {},
     rules: {
       curly: "error",
       eqeqeq: "error",
       "object-shorthand": "error",
       "prefer-const": "error",
+      "@typescript-eslint/ban-types": "off",
+    },
+  },
+  {
+    files: ["**/*.vue"],
+    ignores: [".github", "dist"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "module",
+        parser: tsParser,
+        extraFileExtensions: [".vue"],
+      },
+      globals: {
+        window: "readonly",
+        document: "readonly",
+      },
+    },
+    plugins: {
+      vue,
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      curly: "error",
+      eqeqeq: "error",
+      "object-shorthand": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/ban-types": "off",
     },
   },
   prettier,
