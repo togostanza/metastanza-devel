a7ec65f09329c283105423f2da2d96c4b1130d56
Anton Zhuravlev <nokker@gmail.com>
2025-10-14 16:27:57 +0900

    use selection plugin

--- stanzas/scroll-table/app.vue
+++ stanzas/scroll-table/app.vue
@@ -1,85 +1,100 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
-  <div ref="rootElement" class="wrapper" :style="`width: ${canvasWidth}; height: ${canvasHeight};`">
-    <div
-      class="tableWrapper"
-      @scroll="handleScroll"
-    >
-    <table v-if="state.allRows">
-      <thead ref="thead">
-        <tr>
-          <th
-            v-for="(column, index) in state.columns"
-            :id="column.id"
-            :key="column.id"
-            :class="{ fixed: column.fixed }"
-            :style="
-              column.fixed
-                ? `left: ${index === 0 ? 0 : state.thListWidth[index - 1]}px;`
-                : null
-            "
+  <div
+    ref="rootElement"
+    class="wrapper"
+    :style="`width: ${canvasWidth}; height: ${canvasHeight};`"
+  >
+    <div class="tableWrapper" @scroll="handleScroll">
+      <table v-if="state.allRows">
+        <thead ref="thead">
+          <tr>
+            <th
+              v-for="(column, index) in state.columns"
+              :id="column.id"
+              :key="column.id"
+              :class="{ fixed: column.fixed }"
+              :style="
+                column.fixed
+                  ? `left: ${index === 0 ? 0 : state.thListWidth[index - 1]}px;`
+                  : null
+              "
+            >
+              {{ column.label }}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="(row, row_index) in state.allRows"
+            :key="row.id"
+            :data-id="getRowDataId(row)"
+            :class="{
+              selected: isSelectedRow(row),
+              selectable: eventOutgoingChangeSelectedNodes,
+            }"
           >
-            {{ column.label }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(row, row_index) in state.allRows" :key="row.id">
-          <td
-            v-for="(cell, index) in row"
-            :key="cell.column.id"
-            :class="[
-              cell.column.align,
-              { fixed: cell.column.fixed },
-              cell.column.class,
-            ]"
-            :style="
-              cell.column.fixed
-                ? `left: ${index === 0 ? 0 : state.thListWidth[index - 1]}px;`
-                : null
-            "
-          >
-            <span v-if="cell.href">
-              <AnchorCell
-                :id="`${cell.column.id}_${row_index}`"
-                :href="cell.href"
-                :value="cell.value"
-                :target="cell.target ? `_${cell.target}` : '_blank'"
-                :unescape="cell.unescape"
-                :line-clamp="cell.lineClamp"
-              />
-            </span>
-            <span v-else-if="cell.lineClamp">
-              <LineClampCell
-                :id="`${cell.column.id}_${row_index}`"
-                :value="cell.value"
-                :unescape="cell.unescape"
-                :line-clamp="cell.lineClamp"
-              />
-            </span>
-            <!-- eslint-disable-next-line vue/no-v-html -->
-            <span v-else-if="cell.unescape" v-html="cell.value"></span>
-            <span v-else>{{ cell.value }}</span>
-          </td>
-        </tr>
-        <tr v-if="state.isFetching">
-          <td
-            :colspan="state.columns.length"
-            class="togostanza-table-loading-wrapper"
-          >
-            <div class="togostanza-table-loading-dots"></div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <div v-if="state.hasError" class="togostanza-table-error-message">
-      {{ message_load_error }}
-    </div>
-    <div v-else-if="state.allRows && state.allRows.length === 0 && !state.isFetching" class="togostanza-table-no-data">
-      {{ message_not_found }}
+            <td
+              v-for="(cell, index) in row"
+              :key="cell.column.id"
+              :class="[
+                cell.column.align,
+                { fixed: cell.column.fixed },
+                cell.column.class,
+              ]"
+              :style="
+                cell.column.fixed
+                  ? `left: ${index === 0 ? 0 : state.thListWidth[index - 1]}px;`
+                  : null
+              "
+              @mousedown="handleMouseDown"
+            >
+              <span v-if="cell.href">
+                <AnchorCell
+                  :id="`${cell.column.id}_${row_index}`"
+                  :href="cell.href"
+                  :value="cell.value"
+                  :target="cell.target ? `_${cell.target}` : '_blank'"
+                  :unescape="cell.unescape"
+                  :line-clamp="cell.lineClamp"
+                />
+              </span>
+              <span v-else-if="cell.lineClamp">
+                <LineClampCell
+                  :id="`${cell.column.id}_${row_index}`"
+                  :value="cell.value"
+                  :unescape="cell.unescape"
+                  :line-clamp="cell.lineClamp"
+                />
+              </span>
+              <!-- eslint-disable-next-line vue/no-v-html -->
+              <span v-else-if="cell.unescape" v-html="cell.value"></span>
+              <span v-else>{{ cell.value }}</span>
+            </td>
+          </tr>
+          <tr v-if="state.isFetching">
+            <td
+              :colspan="state.columns.length"
+              class="togostanza-table-loading-wrapper"
+            >
+              <div class="togostanza-table-loading-dots"></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-if="state.hasError" class="togostanza-table-error-message">
+        {{ message_load_error }}
+      </div>
+      <div
+        v-else-if="
+          state.allRows && state.allRows.length === 0 && !state.isFetching
+        "
+        class="togostanza-table-no-data"
+      >
+        {{ message_not_found }}
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -96,6 +111,7 @@ import LineClampCell from "./LineClampCell.vue";
 import loadData from "togostanza-utils/load-data";
 
 import metadata from "./metadata.json";
+import { METASTANZA_NODE_ID_KEY } from "@/lib/MetaStanza";
 
 export default defineComponent({
   components: {
@@ -115,7 +131,7 @@ export default defineComponent({
       allRows: [],
       main: null,
       offset: 0,
-
+      selectedRows: [],
       isFetching: false,
       hasError: false,
 
@@ -136,47 +152,55 @@ export default defineComponent({
           state.offset
         );
 
-      if (params.columns) {
-        state.columns = JSON.parse(params.columns).map((column, index) => {
-          column.fixed = index < params.fixedColumns;
-          return column;
-        });
-      } else if (data.length > 0) {
-        const firstRow = data[0];
-        state.columns = Object.keys(firstRow).map((key, index) => {
-          return {
-            id: key,
-            label: key,
-            fixed: index < params.fixedColumns,
-          };
-        });
-      } else {
-        state.columns = [];
-      }
-
-      state.allRows = state.allRows.concat(
-        data.map((row) => {
-          return state.columns.map((column) => {
+        let columns;
+        if (params.columns) {
+          columns = JSON.parse(params.columns).map((column, index) => {
+            column.fixed = index < params.fixedColumns;
+            return column;
+          });
+        } else if (data.length > 0) {
+          const firstRow = data[0];
+          columns = Object.keys(firstRow).map((key, index) => {
             return {
-              column,
-              value: row[column.id],
-              href: column.link ? row[column.link] : null,
-              unescape: column.escape === false,
-              align: column.align,
-              class: column.class,
-              target: column.target,
-              lineClamp: column["line-clamp"],
+              id: key,
+              label: key,
+              fixed: index < params.fixedColumns,
             };
           });
-        })
-      );
-
-      state.isFetching = false;
-      } catch (error) {
-        if (process.env.NODE_ENV === 'development') {
-          console.error('Failed to fetch data:', error);
         } else {
-          console.error('Failed to fetch data.');
+          columns = [];
+        }
+
+        columns.push({ id: METASTANZA_NODE_ID_KEY, label: "", fixed: false });
+
+        state.columns = columns;
+
+        state.allRows = state.allRows.concat(
+          data.map((row) => {
+            return state.columns.map((column) => {
+              return {
+                column,
+                value:
+                  column.id === METASTANZA_NODE_ID_KEY
+                    ? String(row[column.id])
+                    : row[column.id],
+                href: column.link ? row[column.link] : null,
+                unescape: column.escape === false,
+                align: column.align,
+                class: column.class,
+                target: column.target,
+                lineClamp: column["line-clamp"],
+              };
+            });
+          })
+        );
+
+        state.isFetching = false;
+      } catch (error) {
+        if (process.env.NODE_ENV === "development") {
+          console.error("Failed to fetch data:", error);
+        } else {
+          console.error("Failed to fetch data.");
         }
         state.hasError = true;
         state.isFetching = false;
@@ -200,18 +224,44 @@ export default defineComponent({
       }
     }
 
+    const handleMouseDown = (event) => {
+      if (event.shiftKey) {
+        event.preventDefault();
+      }
+    };
+
+    const getRowDataId = (row) => {
+      return row.find((column) => column.column.id === METASTANZA_NODE_ID_KEY)
+        .value;
+    };
+
     onMounted(() => {
       // Read CSS variables and set canvas size
       requestAnimationFrame(() => {
         const style = window.getComputedStyle(rootElement.value);
-        const widthFromCss = style.getPropertyValue("--togostanza-canvas-width").trim();
+        const widthFromCss = style
+          .getPropertyValue("--togostanza-canvas-width")
+          .trim();
         canvasWidth.value = widthFromCss ? widthFromCss + "px" : "100%";
-        const heightFromCss = style.getPropertyValue("--togostanza-canvas-height").trim();
+        const heightFromCss = style
+          .getPropertyValue("--togostanza-canvas-height")
+          .trim();
         canvasHeight.value = heightFromCss ? heightFromCss + "px" : "";
       });
 
       fetchData();
     });
+
+    const isSelectedRow = (row) => {
+      const rowID = row.find(
+        (column) => column.column.id === METASTANZA_NODE_ID_KEY
+      ).value;
+      return state.selectedRows.includes(String(rowID));
+    };
+
+    const updateSelectedRows = (ids) => {
+      state.selectedRows = ids;
+    };
 
     const thead = ref(null);
     onRenderTriggered(() => {
@@ -224,12 +274,18 @@ export default defineComponent({
     return {
       state,
       handleScroll,
+      handleMouseDown,
+      updateSelectedRows,
+      isSelectedRow,
+      getRowDataId,
       rootElement,
       canvasWidth,
       canvasHeight,
       thead,
       message_not_found,
       message_load_error,
+      eventOutgoingChangeSelectedNodes:
+        params.eventOutgoing_change_selected_nodes,
     };
   },
 });
